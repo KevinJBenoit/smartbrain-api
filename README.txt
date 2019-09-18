155eacbe0fd2cef0eef36783fb6289e936b31173
Kevin Benoit <kevin.benoit8@gmail.com>
2019-09-18 10:43:03 -0600

    all files

---
+++ README.txt
@@ -0,0 +1,8 @@
+Final project for Udemy course
+Thank you to Andrei for your guidance in web dev. Looking forward to using my 
+new knowledge to build some awesome projects!
+
+Clone this repo
+Run npm install
+Run npm start
+** Make sure you use postgreSQL instead of mySQL for this code base.
---
+++ server.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'pmtuv41!49',
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res)=> {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res)=>{
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            } 
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res)=>{
+    const { email, name, password } = req.body;
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json("unable to register"))
+})
+
+//the :id is the parameter
+app.get('/profile/:id', (req, res)=>{
+    const { id } = req.params;
+
+    db.select('*').from('users').where({
+        id: id
+    })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            }
+            else {
+                res.status(400).json("Not found")
+            }
+        })
+        .catch(err => res.status(400).json("Error getting user"))
+})
+
+
+app.put('/image', (req, res)=>{
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+})
+
+
+// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+//     // Store hash in your password DB.
+//   });
+
+// // Load hash from your password DB.
+// bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
+//     // res == false
+// });
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
+
+//1st:
+// root res = this is working
+
+//2nd:
+//signin -> POST, respond with success//fail
+            //POST because you want to send a secured password in the body
+
+//register -> POST, respond with user
+
+//profile with optional parameter of :userId --> GET, respond with user
+
+//image --> PUT return updated user
